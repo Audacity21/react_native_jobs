d89361ef077244ca26f0d20e3ca87e58c0b1ba20
Audacity21 <iam4nkit@gmail.com>
2023-07-24 16:54:23 +0530

    implemented popular jobs component

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': 'bc93fdd43amsh70ae730ba009709p12a0a4jsncff653311958',
+      'X-,RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch {
+      setError(error);
+      alert('Something went wrong!');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
